489bdaf470ee3aa3975a8cff486ca8cd17869a63
Vladyslav Kostiuk <K4nezoor@gmail.com>
2025-08-28 10:52:02 +0200

    added clearer value names

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,17 +8,18 @@
  */
 function transformStateWithClones(state, actions) {
   const results = [];
+
   let clonedState = { ...state };
 
   for (const action of actions) {
     if (action.type === 'addProperties') {
-      const addState = { ...clonedState };
+      const addPropState = { ...clonedState };
 
       for (const key in action.extraData) {
-        addState[key] = action.extraData[key];
+        addPropState[key] = action.extraData[key];
       }
 
-      clonedState = addState;
+      clonedState = addPropState;
     }
 
     if (action.type === 'removeProperties') {
